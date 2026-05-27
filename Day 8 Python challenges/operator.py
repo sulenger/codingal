9a8d0de09926ec13fe46c6eaebf2bf02b0dc547e
sulenger <sulenger99@gmail.com>
2026-05-27 19:16:57 +0200

    .

---
+++ Day 8 Python challenges/operator.py
@@ -0,0 +1,18 @@
+v = 4
+w = 5
+x = 8
+y = 2
+z = 0
+z = (v+w) * x / y;
+print("Value of (v+w) * x/ y is", z)
+
+
+
+
+name = "John"
+age = 0
+
+if name == "Alex" or name == "John" and age >= 2:
+    print("Hello! Welcome.")
+else: 
+    print ("Good Bye!!")
