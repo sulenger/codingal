a0fae14ee11e326f84baf1349af257a98f45e4b7
sulenger <sulenger99@gmail.com>
2026-06-17 19:08:25 +0200

    .

---
+++ Day 13 Patterns/Floyd's Triangle.py
@@ -0,0 +1,9 @@
+row = int(input("please enter the number of rows:  "))
+number = 1
+print(" Floyd's Triangle")
+
+for i in range (1,row + 1):
+    for j in range(1, i +1):
+        print(number,end = " ")
+        number = number + 1 
+    print()
