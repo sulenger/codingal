e04c4588a5ad271e0cb91aabd9ff63afbd0c6183
sulenger <sulenger99@gmail.com>
2026-08-19 21:25:26 +0530

    .

---
+++ Day 21 Tuple/operation.py
@@ -0,0 +1,19 @@
+tuplex = ("tuple", False , 3.2,1)
+print(tuplex)
+tuplex = (4,6,2,8,3,1)
+print(tuplex)
+tuplex = tuplex + (9,)
+print(tuplex)
+
+tuple1 = (50,10,60,70,50)
+print(tuple1.count(50))
+
+tuplex = (2,4,3,5,4,6,7,8,6,1)
+
+slice1 = tuplex[3:6]
+print(slice1)
+
+
+n = (12,15,17,18,23,24,25,27)
+slice2 = n[:3]
+print(slice2)
