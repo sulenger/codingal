a1f0afaf4a4527ec48564feaf823cf05f678256c
sulenger <sulenger99@gmail.com>
2026-08-21 22:27:48 +0530

    .

---
+++ Day 22 Dictionary/countrycode.py
@@ -0,0 +1,17 @@
+country_code = {'India' : '0091',
+                'Australia' : '0025',
+                'Nepal' : '00997'}
+
+print("Country code for India -")
+print(country_code.get('India', 'Not Found'))
+
+print("Country code for Japan -")
+print(country_code.get('Japan', 'Not Found'))
+
+print("Country code for Germany -")
+print(country_code.get('Germany', 'Not Found'))
+
+
+
+
+
